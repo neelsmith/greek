21f3af1fbbb5409583625631d386184660422894
Neel Smith <dnsmith.neel@gmail.com>
2017-02-04 11:55:43 -0500

    Test normalization in js environment

--- js/src/test/scala/ExportTest.scala
+++ js/src/test/scala/ExportTest.scala
@@ -2,7 +2,8 @@ package edu.holycross.shot.greek
 
 import org.scalatest._
 import scala.scalajs.js
-//import js.JSStringOps._
+import scala.scalajs.js.UnicodeNormalizationForm._
+import js.JSStringOps._
 
 
 class ExportTest extends FlatSpec {
@@ -10,6 +11,13 @@ class ExportTest extends FlatSpec {
   "The greek library"  should "construct a LiteraryGreekString from a String"  in {
     val litgs = LiteraryGreekString("mh=nin")
     assert (litgs.ascii == "mh=nin")
+  }
+
+  it should "be scaffolded for unicode normalization" in {
+    val s = "string1"
+    val normalized = s.normalize()  //normalize(UnicodeNormalizationForm.NFC
+
+    assert (s == normalized)
   }
 
 }
